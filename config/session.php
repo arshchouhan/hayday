b2500485c44b601219dbabf34e2c1477f8c88b46
Arsh Chouhan <arshchouhan004@gmail.com>
2026-05-05 23:25:11 +0530

    Default sessions to cookie

--- config/session.php
+++ config/session.php
@@ -18,9 +18,7 @@ return [
     |
     */
 
-    'driver' => env('DB_CONNECTION', 'mysql') === 'mongodb'
-        ? 'cookie'
-        : env('SESSION_DRIVER', 'database'),
+    'driver' => env('SESSION_DRIVER', 'cookie'),
 
     /*
     |--------------------------------------------------------------------------
