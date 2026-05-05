4b15537094fecc4e029722cf912eb71abc5b7eca
Arsh Chouhan <arshchouhan004@gmail.com>
2026-05-05 23:20:37 +0530

    Use cookie sessions on MongoDB deployments

--- config/session.php
+++ config/session.php
@@ -18,7 +18,9 @@ return [
     |
     */
 
-    'driver' => env('SESSION_DRIVER', 'database'),
+    'driver' => env('DB_CONNECTION', 'mysql') === 'mongodb'
+        ? 'cookie'
+        : env('SESSION_DRIVER', 'database'),
 
     /*
     |--------------------------------------------------------------------------
